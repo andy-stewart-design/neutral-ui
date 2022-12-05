3a54deba6995e327beaa99a55132f11093115d80
Andy Stewart <andy.stewart1170@gmail.com>
2022-12-05 09:56:51 -0800

    reverting radio component names

--- src/lib/radio-group/index.ts
+++ src/lib/radio-group/index.ts
@@ -1,4 +1,4 @@
 export { default as RadioGroup } from './RadioGroup.svelte';
-export { default as RadioOption } from './RadioGroupOption.svelte';
-export { default as RadioLabel } from './RadioGroupLabel.svelte';
+export { default as RadioGroupOption } from './RadioGroupOption.svelte';
+export { default as RadioGroupLabel } from './RadioGroupLabel.svelte';
 // export { default as RadioGroupDescription } from "../description/Description.svelte";
