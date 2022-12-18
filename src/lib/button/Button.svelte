fabcdc1dfab4067e79553e950558ebac3d159cb8
Andy Stewart <andy.stewart1170@gmail.com>
2022-12-18 15:45:47 -0800

    making tab index a prop of btn component

--- src/lib/button/Button.svelte
+++ src/lib/button/Button.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	export let label: string | undefined = undefined;
 	export let disabled = false;
+	export let tabindex = '0';
 	export { className as class };
 
 	let className = '';
@@ -8,18 +9,21 @@
 	const ariaID = crypto.randomUUID().split('-').pop()!;
 	const role = 'button';
 	const id = `nui-${role}-${ariaID}`;
-	const tabindex = '0';
 
 	let attributes: {} = { id, role, type: role, tabindex };
 	if (label) attributes = { name: label, ['aria-label']: label, ...attributes };
 </script>
 
 <button {...attributes} class={className} on:click {disabled} aria-disabled={disabled}>
-	<span style:display="block" aria-hidden="true">
-		<slot name="start" />
-	</span>
+	{#if $$slots.start}
+		<span style:display="block" aria-hidden="true">
+			<slot name="start" />
+		</span>
+	{/if}
 	<slot />
-	<span style:display="block" aria-hidden="true">
-		<slot name="end" />
-	</span>
+	{#if $$slots.end}
+		<span style:display="block" aria-hidden="true">
+			<slot name="end" />
+		</span>
+	{/if}
 </button>
